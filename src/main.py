c9e2c0320642b6a5557d335b716c4dae4615af86
Paris Descartes <Descartes@wifi-pro-172-30-41-67.wifi.univ-paris5.fr>
2019-02-19 16:06:55 +0100

    Tested matplotlib

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 
+# We need to change the used backend to not rely on the system one
+import matplotlib
+matplotlib.use('Qt5Agg')
+
 from PyQt5 import QtWidgets as widgets
+import matplotlib.pyplot as plt
+
 
 if __name__ == "__main__":
+
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
+
     app = widgets.QApplication([])
     label = widgets.QLabel('Hello World')
     label.show()
