7c58abdf6ef08ab9093991963ddd32a2454646a8
Paris Descartes <Descartes@Sofianes-MacBook-Pro.local>
2019-03-11 18:56:13 +0100

    Updated tests with latest Point API

--- src/tests/main.py
+++ src/tests/main.py
@@ -4,7 +4,7 @@ def test_segments(segments, height, affiche = False):
     result = np.zeros((height, height))
     for segment in segments:
         for point in segment:
-            result[point[0], point[1]] += 1
+            result[point.x, point.y] += 1
     if affiche:
         print(result)
     return np.array_equal(np.ones((height, height)), result)
