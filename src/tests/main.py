e91b1a98dfa8a2f1097ae86b89a1ebaa5a6a593f
Paris Descartes <Descartes@Sofianes-MacBook-Pro.local>
2019-03-11 13:40:44 +0100

    _format

--- src/tests/main.py
+++ src/tests/main.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def test_segments(segments, height, affiche=False):
+def test_segments(segments, height, affiche = False):
     result = np.zeros((height, height))
     for segment in segments:
         for point in segment:
-            result[point[0], point[1]] +=1
+            result[point[0], point[1]] += 1
     if affiche:
         print(result)
     return np.array_equal(np.ones((height, height)), result)
