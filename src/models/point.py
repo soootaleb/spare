7e9b69c56abb3d424339b4a583419aaa94d4d11b
Paris Descartes <Descartes@Sofianes-MacBook-Pro.local>
2019-04-04 17:21:13 +0200

    Added Point::tuple property

--- src/models/point.py
+++ src/models/point.py
@@ -5,12 +5,16 @@ class Point(object):
     x = None
     y = None
 
+    tuple = tuple()
+
     def __init__(self, x, y):
         if x < 0: raise ValueError('Point::new - X must be positive instead of {}'.format(self.x))
         if y < 0: raise ValueError('Point::new - Y must be positive instead of {}'.format(self.y))
         
         self.x = x
         self.y = y
+
+        self.tuple = (x, y)
 
     def __str__(self):
         return str([self.x, self.y])
