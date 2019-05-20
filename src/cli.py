fcb52a702a7fbfc59b617b1b03b65e2a98a17b92
Paris Descartes <Descartes@wifi-pro-172-30-41-67.wifi.univ-paris5.fr>
2019-05-20 15:25:55 +0200

    CLI example

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,13 @@
-import argparse
+import click
 
-if __name__ == "__main__":
-    print('CLIK')
+@click.command()
+@click.option("--count", default=1, help="Number of greetings.")
+@click.option("--name", prompt="Your name",
+              help="The person to greet.")
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for _ in range(count):
+        click.echo("Hello, %s!" % name)
+
+if __name__ == '__main__':
+    hello()
